a5c60a03551b5f8d51711d30e0277165915cc7f2
kubodev <moisesmagna@gmail.com>
2020-03-28 11:57:48 -0300

    terminando lo basico de php

---
+++ 22-subidas/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Subir archivos con PHP</h1>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="archivo">
+        <input type="submit" value="Enviar">
+    </form>
+
+    <h1>Listado de imagenes</h1>
+    <?php
+        $gestor = opendir('./images');
+
+        if($gestor):
+        
+            while(($image = readdir($gestor)) !== false):
+            
+                if($image != '.' && $image != '..'):
+                
+                    echo "<img src='images/$image' width='200px'<br>";
+                endif;
+            endwhile;
+        
+        endif;
+
+
+
+    ?>
+
+
+</body>
+</html>
